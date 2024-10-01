9d0b48e73741c6f137c31c847868b14ca4d208c3
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-10-01 13:23:12 +0200

    First draft Nivell 2 Exercici 6

---
+++ Nivell2/Exercici6/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        accesElementArray();
+    }
+    public static void accesElementArray(){
+        String[] array = {"A", "B", "C", "D", "E", "F"};
+        String a = array[6];
+    }
+}
